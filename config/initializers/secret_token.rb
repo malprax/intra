d08d64c06cdd8b92cba62290dae1f06565d155e9
aulia <kingmalprax@gmail.com>
2013-09-11 16:01:37 +0800

    create scaffold pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Intra::Application.config.secret_key_base = '70fce58c2667bbfb27f81233b3e1a53eaf160cb43dc914895c15e616b4d2aa819870ce49cedd0416db83414afd54a42921aa2cff39007802aeb5673caf36e7c5'
